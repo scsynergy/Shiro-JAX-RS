ae975294b6d683b717508e1572a811d0dae3e9d4
scsynergy <ronald.feicht@scsynergy.de>
2023-05-05 09:20:49 +0200

    Create ApplicationConfig.java

---
+++ src/main/java/de/scsynergy/jax/rs/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package de.scsynergy.jax.rs;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class ApplicationConfig extends Application {
+    
+}
